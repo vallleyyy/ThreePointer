d1632340d9a928eae0691a458cbceb11abca5aa7
Valerie Pope <valeriepope315@gmail.com>
2026-06-13 23:06:16 -0600

    added new shooter model

--- shooter.py
+++ shooter.py
@@ -7,17 +7,16 @@ class Shooter(pygame.sprite.Sprite):
         self.name = name
         self.number = number
         self.team = team
-        self.image = pygame.image.load("assets/sample3.jpg").convert()
+        self.image = pygame.image.load("assets/shooter.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = 100
-        self.rect.y = 300
+        self.rect.y = 50
 
     def update(self, time_passed, shot_timer, shooting):
         if shooting:
-            if self.rect.y > 220:
+            if self.rect.y > -30:
                 self.rect.move_ip(0, -5)
         else:
-            if self.rect.y < 300:
+            if self.rect.y < 50:
                 self.rect.move_ip(0, 10)
 
     def draw(self, surface):
